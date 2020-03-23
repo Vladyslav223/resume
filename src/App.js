ab0e6bac5339678727375ea67f2845f5e75a364f
vladyslav223 <vladyslav223@gmail.com>
2020-03-23 15:37:27 +0200

    homepage changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="cup.png" className="App-logo" alt="logo" />
+        <img src="img/cup.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
